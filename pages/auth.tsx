624deb279bb2f4a22445773fdd88bd15cc1e29d9
DevAbdelilah <charafiabdelilah3@gmail.com>
2024-07-02 05:20:57 +0200

    add if user login go to home page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,8 +2,11 @@
 import React, { useState, useCallback } from "react";
 import Input from "@/components/Input";
 import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
-export default function Home() {
+export default function Auth() {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState(""); // Changed from username to name
   const [password, setPassword] = useState("");
@@ -15,17 +18,33 @@ export default function Home() {
     );
   }, []);
 
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
   const registre = useCallback(async () => {
     try {
       await axios.post("/api/registre", {
         email,
-        name, 
+        name,
         password,
       });
+      login();
     } catch (error) {
       console.log(error);
     }
-  }, [email, name, password]); 
+  }, [email, name, password, login]);
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -74,7 +93,7 @@ export default function Home() {
               />
             </div>
             <button
-              onClick={registre}
+              onClick={variant === "login" ? login : registre}
               className="bg-red-600 w-full py-3 text-white rounded-md mt-10 hover:bg-red-700 transition"
             >
               {variant === "login" ? "login" : "Registre"}
